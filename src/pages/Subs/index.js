ddbfced4c095683ded4f86c11a4866ea77431694
Augusto Garcia da Rosa <augusto.garciadarosa@gmail.com>
2021-03-27 01:31:22 -0300

    correção da data 3

--- src/pages/Subs/index.js
+++ src/pages/Subs/index.js
@@ -17,7 +17,11 @@ export default function Subs(){
 
     function tratadata(date){
         var data = new Date(date);
-        return(data.toLocaleDateString('pt-BR', {timeZone: 'UTC'}));
+        var d =(data.getDate()+1).toString();
+        var m =(data.getMonth()+1).toString();
+        var dia = (d.length === 1) ? '0'+d:d;
+        var mes = (m.length === 1) ? '0'+m:m;
+        return(dia+'/'+mes+'/'+data.getFullYear());
     }
 
     useEffect(()=>{
